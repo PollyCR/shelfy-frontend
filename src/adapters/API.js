40b026ec46b3f5d39c335c51c21a31d3ac18a959
PollyCR <polly.c.reynolds@gmail.com>
2020-01-18 12:44:27 +0000

    changed url to test netlify.toml

--- src/adapters/API.js
+++ src/adapters/API.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/";
+const BASE_URL = "https://shelfy-backend.netlify.com";
 
 const LOGIN_URL = `${BASE_URL}api/v1/login`;
 const SIGNUP_URL = `${BASE_URL}api/v1/signup`;
